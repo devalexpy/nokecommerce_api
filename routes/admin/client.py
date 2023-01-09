611f994a93d342fed7b82e9a0e51fafc0b1a9d1d
alex <alexarroyo9912@gmail.com>
2023-01-09 15:29:01 -0500

    :hammer: New admin route for get clients added
    Get all clients route added

--- routes/admin/client.py
+++ routes/admin/client.py
@@ -1,9 +1,12 @@
 from fastapi import (
     APIRouter, status, Depends,
-    HTTPException
+    HTTPException, Query
 )
+from schemas.clients import ClientBase
 from security import get_admin
 from db.admin_queries import get_admin_by_id
+from db.clients_queries import get_all_clients
+from typing import List, Optional
 
 router = APIRouter(
     prefix="/admin/client",
@@ -14,6 +17,13 @@ router = APIRouter(
 @router.get(
     path="/",
     status_code=status.HTTP_200_OK,
+    response_model=List[ClientBase]
 )
-async def get_clients(admin=Depends(get_admin)):
-    pass
+async def get_clients(admin=Depends(get_admin), limit: Optional[int] = Query(None)):
+    clients = await get_all_clients(limit)
+    if clients is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Clients not found",
+        )
+    return clients
